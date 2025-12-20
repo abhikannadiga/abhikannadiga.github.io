3fca83fada83bdc2a997f574b57e86cc224bbed3
ABHISHEK N N <nnabhn1@gmail.com>
2025-12-20 23:26:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Website Loaded');
+
+document.addEventListener("scroll",()=>{
+    document.querySelectorAll('.fade-up').forEach(el=>{
+        let top = el.getBoundingClientRect().top;
+        if(top < window.innerHeight - 60){
+            el.classList.add("visible");
+        }
+    });
+});
