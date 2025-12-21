e692f9344849b042871e01ebfb982aeb2de8f74d
ABHISHEK N N <nnabhn1@gmail.com>
2025-12-21 10:13:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Animations Ready');
+console.log('Website Loaded');
+var acc=document.getElementsByClassName('accordion-btn');
+for(let i=0;i<acc.length;i++){
+ acc[i].addEventListener('click',function(){
+   var panel=this.nextElementSibling;
+   panel.style.display = panel.style.display==='block' ? 'none' : 'block';
+ });
+}
