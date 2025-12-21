30ab5ae300a73751a776e2303edcdf3271308e46
ABHISHEK N N <nnabhn1@gmail.com>
2025-12-21 19:13:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-console.log('Website Loaded');
-var acc=document.getElementsByClassName('accordion-btn');
-for(let i=0;i<acc.length;i++){
- acc[i].addEventListener('click',function(){
-   var panel=this.nextElementSibling;
-   panel.style.display = panel.style.display==='block' ? 'none' : 'block';
- });
-}
+console.log('Animations Ready');
